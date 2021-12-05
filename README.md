6aa90d3c6220625a45c8a3ea95957db152db5d3a
terminalarch <57570117+terminalarch@users.noreply.github.com>
2021-12-05 13:43:54 +0100

    Version 1.1
    Real configuration (with the configparser module), slight code refactoring, add proper error handling for missing configparser module?

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # Mario Royale AI
 The very first Mario Royale... AI? No, it's more of an A.<br>
-This new [Python](https://python.org) program rolls a dice and chooses a random input once every .8 seconds.<br>
+This [Python](https://python.org) program rolls a dice and chooses a random input once every .8 seconds.<br>
 ![ScreenShot](assets/bash.png)<br>
 The program logs it's decisions and prints it in the console, and you get to watch it suffer.<br>
 ![ScreenShot](assets/goomba.png)<br>
 There is absolutely zero strategy, good luck having the "AI" pass the 1-1 goomba.
 
+## Version 1.1 Patch Notes
+The software will now show a count.. up before it starts up<br>
+You can now change the input interval from **configuration.cfg**, though it will subtract 0.8 from the value<br>
+Added a config file in general, though not much plans to update it.<br>
+
+## To look forward to?
+Rebinding controls through the configuration<br>
+Proper error handling for no configparser module (even though this should have been done already lol)
+
 ## Installation
-You will need these tools to run the software
+You will need a program and pip to run the software
 - Install [Python](https://python.org) from the official website
 - Download/clone this repository then extract it to a folder, then cd into it
 - Install the **pynput** package from pip3
+- Install the **configparser** package from pip3
 
 ## Notes
 The program will start 5 seconds after being started, so rush into the game before it starts screwing your desktop.<br>
-Good luck. You will need it.
--- main.py
+++ main.py
@@ -4,80 +4,71 @@
 ##     github.com/terminalarch/MarioRoyaleAI   ## ##     Free Open-source software     ##
 ################################################# #######################################
 from pynput.keyboard import Key, Controller
+import configparser
 import time, random, threading
 
 keyboard = Controller()
 print("Awake, waiting 5 seconds")
-time.sleep(5)
+for i in range(5):
+    print(i + 1)
+    time.sleep(1)
 
-while True:
-    right = 0 # times in a row count
-    left = 0 # times in a row count
-    class Random:
-        def init():
-            # Variables used to block an input if it was used too many times in a row
-            global left
-            global right
+# Configuration
+config = configparser.ConfigParser()
+config.read('conf.cfg')
 
-            # Choose a random number
-            res = random.randrange(1, 7)
+class main():
+    def init():
+        # Variables used to block an input if it was used too many times in a row
+        global left
 
-            # Iterate through numbers then choose input
-            if res == 1: # Go left
-                if left > 2:
-                    left = 0
-                    print("Cancelled left input")
-                    return
-                print("A (go left)")
-                keyboard.press('a')
-                time.sleep(0.2)
-                keyboard.release('a')
-                left += 1
-                right = 0
+        # Choose a random number
+        res = random.randrange(1, 9)
 
-            elif res == 2: # Go right
-                if right > 2:
-                    right = 0
-                    print("Cancelled right input")
-                    return
-                print("D (go right)")
+        # Iterate through numbers then choose input
+        if res == 1 or res == 2: # Go right
+            print("D (go right)")
+            keyboard.press('d')
+            time.sleep(0.4)
+            keyboard.release('d')
+            left = 0
+
+        elif res == 3 or res == 4: # Right jump
+            print("Space + D (right jump)")
+            keyboard.press(Key.space)
+            with keyboard.pressed(Key.space):
                 keyboard.press('d')
-                time.sleep(1)
+                time.sleep(0.7)
                 keyboard.release('d')
-                right += 1
-                left = 0
+            keyboard.release(Key.space)
 
-            elif res == 3: # Down
-                print("S (go down)")
-                keyboard.press('s')
-                time.sleep(0.3)
-                keyboard.release('s')
+        elif res == 5: # Go left
+            print("A (left)")
+            keyboard.press('a')
+            time.sleep(0.2)
+            keyboard.release('a')
 
-            elif res == 4: # Jump
-                print("Space (jump)")
-                keyboard.press(Key.space)
-                time.sleep(0.6)
-                keyboard.release(Key.space)
+        elif res == 6: # Jump
+            print("Space (jump)")
+            keyboard.press(Key.space)
+            time.sleep(0.3)
+            keyboard.release(Key.space)
 
-            elif res == 5: # Right jump
-                print("Space + D (right jump)")
-                keyboard.press(Key.space)
-                with keyboard.pressed(Key.space):
-                    keyboard.press('d')
-                    time.sleep(0.7)
-                    keyboard.release('d')
-                keyboard.release(Key.space)
+        elif res == 7: # Pipe down
+            print("S (go down)")
+            keyboard.press('s')
+            time.sleep(0.3)
+            keyboard.release('s')
 
-            elif res == 6: # Left jump
-                print("Space + A (left jump)")
-                keyboard.press(Key.space)
-                with keyboard.pressed(Key.space):
-                    keyboard.press('a')
-                    time.sleep(0.7)
-                    keyboard.release('d')
-                keyboard.release(Key.space)
+        elif res == 8: # Slight speedup/fireball
+            print("Shift (fire)")
+            keyboard.press(Key.shift)
+            time.sleep(0.2)
+            keyboard.release(Key.shift)
 
-    # Activate function every 0.8 seconds
-    e = threading.Event()
-    while not e.wait(0.8):
-        Random.init()
+# Activate function every [time] seconds
+interval = config.getint('Config', 'Interval')
+
+e = threading.Event()
+while not e.wait(int(interval - 0.8)):
+    main.init()
