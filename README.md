5668b2a1eaef30cb1186b617ce702b9f4ccf25de
terminalarch <57570117+terminalarch@users.noreply.github.com>
2022-03-05 17:06:13 +0100

    Version 2.0 updates

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # Mario Royale AI
 The very first Mario Royale... AI? No, it's more of an A.<br>
-This [Python](https://python.org) program rolls a dice and chooses a random input once every .8 seconds.<br>
+This [Python](https://python.org) program rolls a dice and chooses a random input once every X seconds<br>
 ![ScreenShot](assets/bash.png)<br>
 The program logs it's decisions and prints it in the console, and you get to watch it suffer.<br>
 ![ScreenShot](assets/goomba.png)<br>
 There is absolutely zero strategy, good luck having the "AI" pass the 1-1 goomba.
 
-## Version 1.1 Patch Notes
-The software will now show a count.. up before it starts up<br>
-You can now change the input interval from **configuration.cfg**, though it will subtract 0.8 from the value<br>
-Added a config file in general, though not much plans to update it.<br>
-
-## To look forward to?
-Rebinding controls through the configuration<br>
-Proper error handling for no configparser module (even though this should have been done already lol)
+## Version 2.0 Patch Notes
+- Added error handler for when configparser is not installed
+- Changed countup to be a countdown
+- Countdown timer is now customizable
+- Input interval no longer subtracts by 8
+- Code refactoring
+- This project will no longer be receiving updates, since I have ran out of ideas for it.
 
 ## Installation
 You will need a program and pip to run the software
 - Install [Python](https://python.org) from the official website
 - Download/clone this repository then extract it to a folder, then cd into it
 - Install the **pynput** package from pip3
-- Install the **configparser** package from pip3
+- Install the **configparser** package from pip3 **OPTIONAL**
+- Run `python3 main.py` (or `python main.py` if you're on Windows)
 
 ## Notes
-The program will start 5 seconds after being started, so rush into the game before it starts screwing your desktop.<br>
+The program has a countdown before it starts, but it affects your regular desktop aswell, so make sure you're in [the game](https://marioroyale.tk) before it finishes counting down
--- main.py
+++ main.py
@@ -3,72 +3,85 @@
 ##             MARIO ROYALE "AI"               ## ##           GNU LICENSE             ##
 ##     github.com/terminalarch/MarioRoyaleAI   ## ##     Free Open-source software     ##
 ################################################# #######################################
+
+# Import libraries
 from pynput.keyboard import Key, Controller
-import configparser
 import time, random, threading
 
 keyboard = Controller()
-print("Awake, waiting 5 seconds")
-for i in range(5):
-    print(i + 1)
-    time.sleep(1)
 
 # Configuration
-config = configparser.ConfigParser()
-config.read('conf.cfg')
+try:
+    import configparser
+    config = configparser.ConfigParser()
+    config.read('main.cfg')
+    CONFIG_IMPORT = True
+except:
+    print("Could not find configparser module, using fallback to defaults.")
+    CONFIG_IMPORT = False
 
-class main():
-    def init():
-        # Variables used to block an input if it was used too many times in a row
-        global left
+def loadData():
+    if CONFIG_IMPORT == True: # Load customized data
+        interval = config.getint('Config', 'Interval')
+        timer = config.getint('Config', 'Timer')
+    else: # Load default data
+        interval = 1
+        timer = 5
+loadData()
 
-        # Choose a random number
-        res = random.randrange(1, 9)
+def countdown():
+    while timer > 0:
+        print(f"{timer} seconds left")
+        timer -= 1
+        time.sleep(1)
+countdown()
 
-        # Iterate through numbers then choose input
-        if res == 1 or res == 2: # Go right
-            print("D (go right)")
+def app():
+    # Choose a random number
+    res = random.randrange(1, 9)
+
+    # Iterate through numbers then choose input
+    if res == 1 or res == 2: # Go right
+        print("D (go right)")
+        keyboard.press('d')
+        time.sleep(0.4)
+        keyboard.release('d')
+        left = 0
+
+    elif res == 3 or res == 4: # Right jump
+        print("Space + D (right jump)")
+        keyboard.press(Key.space)
+        with keyboard.pressed(Key.space):
             keyboard.press('d')
-            time.sleep(0.4)
+            time.sleep(0.7)
             keyboard.release('d')
-            left = 0
+        keyboard.release(Key.space)
 
-        elif res == 3 or res == 4: # Right jump
-            print("Space + D (right jump)")
-            keyboard.press(Key.space)
-            with keyboard.pressed(Key.space):
-                keyboard.press('d')
-                time.sleep(0.7)
-                keyboard.release('d')
-            keyboard.release(Key.space)
+    elif res == 5: # Go left
+        print("A (left)")
+        keyboard.press('a')
+        time.sleep(0.2)
+        keyboard.release('a')
 
-        elif res == 5: # Go left
-            print("A (left)")
-            keyboard.press('a')
-            time.sleep(0.2)
-            keyboard.release('a')
+    elif res == 6: # Jump
+        print("Space (jump)")
+        keyboard.press(Key.space)
+        time.sleep(0.3)
+        keyboard.release(Key.space)
 
-        elif res == 6: # Jump
-            print("Space (jump)")
-            keyboard.press(Key.space)
-            time.sleep(0.3)
-            keyboard.release(Key.space)
+    elif res == 7: # Pipe down
+        print("S (go down)")
+        keyboard.press('s')
+        time.sleep(0.3)
+        keyboard.release('s')
 
-        elif res == 7: # Pipe down
-            print("S (go down)")
-            keyboard.press('s')
-            time.sleep(0.3)
-            keyboard.release('s')
-
-        elif res == 8: # Slight speedup/fireball
-            print("Shift (fire)")
-            keyboard.press(Key.shift)
-            time.sleep(0.2)
-            keyboard.release(Key.shift)
+    elif res == 8: # Slight speedup/fireball
+        print("Shift (fire)")
+        keyboard.press(Key.shift)
+        time.sleep(0.2)
+        keyboard.release(Key.shift)
 
 # Activate function every [time] seconds
-interval = config.getint('Config', 'Interval')
-
 e = threading.Event()
-while not e.wait(int(interval - 0.8)):
-    main.init()
+while not e.wait(int(interval)):
+    app()
